77ea64121d4f31e44a9f565017ef8e1ff19b6cf7
Cailie Skelton <cskelton707@gmail.com>
2020-05-24 19:06:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 attr_accessor :sender, :receiver, :amount, :status
 
-  def initialize(sender, receiver, amount, status="pending")
+  def initialize(sender, receiver, amount, status)
     @sender = sender
     @receiver = receiver
     @amount = amount
