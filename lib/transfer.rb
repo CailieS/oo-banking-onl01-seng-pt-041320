3dc32a0a392fe6f2e5f520f7de2be33d2e571075
Cailie Skelton <cskelton707@gmail.com>
2020-05-24 18:55:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,19 +14,16 @@ class Transfer
     @sender.valid? && @receiver.valid? ? true : false
   end
 
-
-
-
-
-  def reverse_transfer
-    if @status == "complete"
-      @sender.deposit( @amount ) 
-      @receiver.deposit( @amount * -1)
-      @status = "reversed"
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
-
-end
 
   def reverse_transfer
     if @status == "complete"
