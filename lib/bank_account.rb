d06c2e242a63d69c300bfc604e44cf4392530bc6
Cailie Skelton <cskelton707@gmail.com>
2020-05-24 19:07:51 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,33 +1,27 @@
-class BankAccount
 
+class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
-  def initialize(name, balance=1000, status="open")
-    @name = name
-    @balance = balance
-    @status = status
+  def initialize(account_name)
+    @name = account_name
+    @balance = 1000
+    @status = "open"
   end
 
-  def deposit(amount)
-    self.balance += amount
-  end
-
-  def withdrawal(amount)
-    self.balance -= amount
+  def deposit(money)
+    @balance += money
   end
 
   def display_balance
-    "Your balance is $#{self.balance}."
+    "Your balance is $#{@balance}."
   end
 
   def valid?
-    self.status == "open" && self.balance > 0 ? true : false
+    @status == "open" && @balance > 0 ? true : false
   end
 
   def close_account
-    self.status = "closed"
+    @status = "closed"
   end
-
-
-end	
+end
