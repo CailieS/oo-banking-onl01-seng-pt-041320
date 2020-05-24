95cdf468237f3524872be202d80f5117bbfb032c
Cailie Skelton <cskelton707@gmail.com>
2020-05-24 19:03:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,9 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if execute_transaction.valid?
+    return
+      @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
