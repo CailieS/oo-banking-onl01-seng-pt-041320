249d99c831adff7ae3305cb94ecf02d7848e3092
Cailie Skelton <cskelton707@gmail.com>
2020-05-24 18:55:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,24 +15,6 @@ class Transfer
   end
 
 
-class Transfer
-  # code here
-  attr_accessor :sender, :receiver, :amount, :status, :count
-
-  def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
-    @status = "pending"
-  end
-
-  def both_valid?
-    if sender.valid? && receiver.valid?
-      true
-    else
-      false
-    end
-  end
 
 
 
